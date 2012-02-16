20e4f9f04fb920d4b42c004117aeec70745eb879
talos <irving.krauss@gmail.com>
2012-02-16 18:40:18 -0500

    working rss, improved download, working sample cron

--- download.sh
+++ download.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+PATH=${PATH}:./
+
 # Test the waters.
 wget http://www.nyc.gov/html/nypd/downloads/pdf/traffic_data/bxacc.pdf
 pdftotext -layout bxacc.pdf
-YEAR_MONTH=$(python findmonthyear.py bxacc.txt)
+DIR=data/$(findmonthyear.py bxacc.txt)
 
-if [ -e ${YEAR_MONTH} ]
+if [ -e ${DIR} ]
     then
-    echo Already have data for ${YEAR_MONTH}
+    echo Already have data at ${DIR}
     rm bxacc*
     exit 0
 fi
 
-echo Grabbing data for ${YEAR_MONTH}
+echo Grabbing data for ${DIR}
 
 # We don't have the data yet, grab the rest
 wget http://www.nyc.gov/html/nypd/downloads/pdf/traffic_data/bkacc.pdf
@@ -25,16 +27,17 @@ pdftotext -layout mnacc.pdf
 pdftotext -layout qnacc.pdf
 pdftotext -layout siacc.pdf
 
-python scrapeintersections.py bxacc.txt > bronx.csv
-python scrapeintersections.py bkacc.txt > brooklyn.csv
-python scrapeintersections.py mnacc.txt > manhattan.csv
-python scrapeintersections.py qnacc.txt > queens.csv
-python scrapeintersections.py siacc.txt > staten_island.csv
+scrapeintersections.py bxacc.txt > bronx.csv
+scrapeintersections.py bkacc.txt > brooklyn.csv
+scrapeintersections.py mnacc.txt > manhattan.csv
+scrapeintersections.py qnacc.txt > queens.csv
+scrapeintersections.py siacc.txt > staten_island.csv
 
 # Everything in its proper place
-mkdir ${YEAR_MONTH}
-mkdir ${YEAR_MONTH}/raw
+mkdir -p ${DIR}/raw
 
-mv *.csv ${YEAR_MONTH}
-mv *.txt ${YEAR_MONTH}/raw
-mv *.pdf ${YEAR_MONTH}/raw
+mv *.csv ${DIR}
+mv *.txt ${DIR}/raw
+mv *.pdf ${DIR}/raw
+
+echo Finished grabbing data for ${DIR}
