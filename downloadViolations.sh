fae287aab142c4c5862f547e45505f7234ee9d36
John Krauss <irving.krauss@gmail.com>
2012-08-27 21:00:32 -0400

    fixing directory traversal

--- downloadViolations.sh
+++ downloadViolations.sh
@@ -10,7 +10,7 @@ wget -q ${TEST_URL} -O ${TEST_FILE}.pdf
 pdftotext -layout ${TEST_FILE}.pdf
 ./processViolations.py ${TEST_FILE}.txt > ${TEST_FILE}.csv
 
-DIR=public/data/$(findmonthyear.py ${TEST_FILE}.txt)
+DIR=public/data/violations/$(findmonthyear.py ${TEST_FILE}.csv)
 
 rm -f ${TEST_FILE}*
 
@@ -20,21 +20,24 @@ if [ -e ${DIR} ]
     exit 0
 fi
 
-mkdir -p ${DIR}/raw && pushd $_
+mkdir -p ${DIR}/raw
+RAW_DIR=$_
 
 # Iterate over all violations
 for URL in $(cat ${VIOLATIONS_PDFS})
 do
+    pushd ${RAW_DIR}
     echo "Downloading ${URL}"
     wget -q ${URL}
+    popd
 done
 
+pushd ${RAW_DIR}
 for PDF in $(ls *.pdf)
 do
     echo "Proccesing PDF ${PDF}"
     pdftotext -layout ${PDF}
 done
-
 popd
 
 ./processViolations.py ${DIR}/raw/*.txt > ${DIR}/processed.csv
