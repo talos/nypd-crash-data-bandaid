a46a9a0099ce10977a743e16af84bc62c425ac4c
John Krauss <irving.krauss@gmail.com>
2013-07-23 22:13:53 -0400

    updates to the main script

--- bandaid.sh
+++ bandaid.sh
@@ -2,6 +2,10 @@
 
 PUBLIC="public"
 DATA="${PUBLIC}/data"
+TMP="tmp"
+
+TMP_COLLISIONS="${TMP}/collisions.txt"
+TMP_SUMMONS="${TMP}/summons.txt"
 
 COLLISIONS_CSV="${PUBLIC}/collisions.txt"
 SUMMONS_CSV="${PUBLIC}/summons.txt"
@@ -9,9 +13,28 @@ SUMMONS_CSV="${PUBLIC}/summons.txt"
 COLLISIONS_ARCHIVE="${DATA}/collisions.txt.gz"
 SUMMONS_ARCHIVE="${DATA}/summons.txt.gz"
 
-./bandaid/download.py ${RAW_DATA} 2011 8
-./bandaid/processCollisions.py ${RAW_DATA}/*/*/*acc.xlsx > ${COLLISIONS_CSV}
-./bandaid/processSummons.py ${RAW_DATA}/*/*/*sum.xlsx > ${SUMMONS_CSV}
+START_YEAR=2011
+START_MONTH=8
 
+mkdir -p ${PUBLIC}
+mkdir -p ${DATA}
+mkdir -p ${TMP}
+
+echo "Downloading data..."
+./bandaid/download.py ${DATA} ${START_YEAR} ${START_MONTH}
+
+echo "Processing collisions..."
+./bandaid/processCollisions.py ${DATA}/*/*/*acc.xlsx > ${TMP_COLLISIONS}
+mv ${TMP_COLLISIONS} ${COLLISIONS_CSV}
+
+echo "Processing summons..."
+./bandaid/processSummons.py ${DATA}/*/*/*sum.xlsx > ${TMP_SUMMONS}
+mv ${TMP_SUMMONS} ${SUMMONS_CSV}
+
+echo "Zipping files for downloads folder..."
 gzip -c -9 ${COLLISIONS_CSV} > ${COLLISIONS_ARCHIVE}
 gzip -c -9 ${SUMMONS_CSV} > ${SUMMONS_ARCHIVE}
+
+echo "Committing changes to git..."
+git add ${COLLISIONS_CSV} ${SUMMONS_CSV} ${PUBLIC}/intersections.txt
+git commit -m "$(date): Auto-updating data"
